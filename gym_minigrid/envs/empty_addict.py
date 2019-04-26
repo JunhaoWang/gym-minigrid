9bf44dce9388a5638e20cde56891e0994dd99009
Léonard Boussioux <leonard.boussioux@student.ecp.fr>
2019-04-26 15:29:15 -0400

    added new envs

---
+++ gym_minigrid/envs/empty_addict.py
@@ -0,0 +1,92 @@
+from gym_minigrid.minigrid import *
+from gym_minigrid.register import register
+
+class EmptyLavaEnv(MiniGridEnv):
+    """
+    Empty grid environment, no obstacles, sparse reward
+    """
+
+    def __init__(
+        self,
+        size=8,
+        agent_start_pos=(1,1),
+        agent_start_dir=0,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        super().__init__(
+            grid_size=size,
+            max_steps=4*size*size,
+            # Set this to True for maximum speed
+            see_through_walls=True
+        )
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place a goal square in the bottom-right corner
+        self.grid.set(width - 2, height - 2, Goal())
+        self.grid.set(1,height - 2, Lava())
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.start_pos = self.agent_start_pos
+            self.start_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "get to the green goal square"
+
+class EmptyAddictEnv(MiniGridEnv):
+    """
+    Empty grid environment, no obstacles, sparse reward
+    """
+
+    def __init__(
+        self,
+        size=8,
+        agent_start_pos=(1,1),
+        agent_start_dir=0,
+    ):
+        self.agent_start_pos = agent_start_pos
+        self.agent_start_dir = agent_start_dir
+
+        super().__init__(
+            grid_size=size,
+            max_steps=4*size*size,
+            # Set this to True for maximum speed
+            see_through_walls=True
+        )
+
+    def _gen_grid(self, width, height):
+        # Create an empty grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.wall_rect(0, 0, width, height)
+
+        # Place a goal square in the bottom-right corner
+        #self.grid.set(width - 2, height - 2, Goal())
+        self.grid.set(1,height - 2, Goal())
+        # Place the agent
+        if self.agent_start_pos is not None:
+            self.start_pos = self.agent_start_pos
+            self.start_dir = self.agent_start_dir
+        else:
+            self.place_agent()
+
+        self.mission = "get to the green goal square"
+
+register(
+    id='MiniGrid-Empty-8x8-Lava-Goal-v0',
+    entry_point='gym_minigrid.envs:EmptyLavaEnv'
+)
+
+register(
+    id='MiniGrid-Empty-8x8-GoalOnly-v0',
+    entry_point='gym_minigrid.envs:EmptyAddictEnv'
+)

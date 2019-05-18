0478a196d6c38fc00ccb4980a8a34ff5e36a1ab9
Léonard Boussioux <leonard.boussioux@student.ecp.fr>
2019-05-18 10:06:04 -0400

    added multiplace goal env

--- gym_minigrid/envs/fourroomsaddict.py
+++ gym_minigrid/envs/fourroomsaddict.py
@@ -73,6 +73,69 @@ class FourRoomsAddict2Env(MiniGridEnv):
         obs, reward, done, info = MiniGridEnv.step(self, action)
         return obs, reward, done, info
 
+class FourRoomsAddictMovingGoalRoom2Env(MiniGridEnv):
+    """
+    Classic 4 rooms gridworld environment.
+    Can specify agent and goal position, if not it set at random.
+    """
+
+    def __init__(self, agent_pos=(1,1), goal_pos=None):
+        self._agent_default_pos = agent_pos
+        self._goal_default_pos = goal_pos
+        super().__init__(grid_size=11, max_steps=100)
+
+    def _gen_grid(self, width, height):
+        # Create the grid
+        self.grid = Grid(width, height)
+
+        # Generate the surrounding walls
+        self.grid.horz_wall(0, 0)
+        self.grid.horz_wall(0, height - 1)
+        self.grid.vert_wall(0, 0)
+        self.grid.vert_wall(width - 1, 0)
+
+        room_w = width // 2
+        room_h = height // 2
+
+        # For each row of rooms
+        for j in range(0, 2):
+
+            # For each column
+            for i in range(0, 2):
+                xL = i * room_w
+                yT = j * room_h
+                xR = xL + room_w
+                yB = yT + room_h
+
+                # Bottom wall and door
+                if i + 1 < 2:
+                    self.grid.vert_wall(xR, yT, room_h)
+                    pos = (xR, yT + 3)#self._rand_int(yT + 1, yB))
+                    self.grid.set(*pos, None)
+
+                # Bottom wall and door
+                if j + 1 < 2:
+                    self.grid.horz_wall(xL, yB, room_w)
+                    #pos = (self._rand_int(xL + 1, xR), yB)
+                    pos = (xL+3, yB)
+                    self.grid.set(*pos, None)
+
+        # Randomize the player start position and orientation
+        if self._agent_default_pos is not None:
+            self.start_pos = self._agent_default_pos
+            self.grid.set(*self._agent_default_pos, None)
+            self.start_dir = self._rand_int(0, 4)  # assuming random start direction
+        else:
+            self.place_agent()
+
+        self.place_obj(Goal(), top = (1,7), size = (4,4))
+
+        self.mission = 'Reach the goal'
+
+    def step(self, action):
+        obs, reward, done, info = MiniGridEnv.step(self, action)
+        return obs, reward, done, info
+
 class FourRoomsAddictEnv(MiniGridEnv):
     """
     Classic 4 rooms gridworld environment.
@@ -254,3 +317,9 @@ register(
     id='MiniGrid-FourRoomsAddict5-v0',
     entry_point='gym_minigrid.envs:FourRoomsAddict6Env'
 )
+
+register(
+    id='MiniGrid-FourRoomsAddictMovingGoalRoom2-v0',
+    entry_point='gym_minigrid.envs:FourRoomsAddictMovingGoalRoom2Env'
+)
+
